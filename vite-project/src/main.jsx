51e4255f6144ca2049e86ef80e87fe0abbdcd01b
AKHIL SAURABH <akhilsaurabh65@gmail.com>
2026-03-02 23:36:45 +0530

    Replace HashRouter with BrowserRouter in main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 )
